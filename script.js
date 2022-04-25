58ce4ce730d50699e2f8bd6c1d49daac83beca1b
Lucas Almeida <86491146+aelucas@users.noreply.github.com>
2022-04-25 20:22:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function verMais(){
     var pontos = document.getElementById("pontos");
     var maisTexto = document.getElementById("texto-oculto");
     var btnmostrar = document.getElementById("btnmostrar");
+    var btnmostrar = document.querySelector(".discussoes").style.top = "1150px";
 
     if(pontos.style.display === "none"){
         pontos.style.display= "inline";
         maisTexto.style.display ="none";
         btnmostrar.innerHTML = "ver Mais";
+        
 
 
     }else{

e0f0239dd540423e3ef74a665ed9d0daf11824c6
Lucas Almeida <lucas-owned@hotmail.com>
2022-04-27 14:48:03 -0300

    quase acabando com problema na div3 hidden

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function verMais(){
     var maisTexto = document.getElementById("texto-oculto");
     var btnmostrar = document.getElementById("btnmostrar");
     var btnmostrar = document.querySelector(".discussoes").style.top = "1150px";
-
+    var btnmostrar = document.querySelector(".img-footer").style.top = "2100px";
     if(pontos.style.display === "none"){
         pontos.style.display= "inline";
         maisTexto.style.display ="none";
@@ -17,3 +17,44 @@ function verMais(){
         btnmostrar.innerHTML = "ver menos";
     }
 }
+
+
+function toggleDiv() {
+  
+
+    if(document.getElementById("div1").style.display == 'none') {
+      document.getElementById("div1").style.display = 'block';
+      document.getElementById("div2").style.display = 'none';
+      document.querySelector(".perg-marg").style.top = "35px 0 0 15px";
+      document.querySelector(".img-footer").style.top = "1750px";
+
+      
+      
+      
+    } else {
+      document.getElementById("div1").style.display = 'none';
+      document.getElementById("div2").style.display = 'block';
+      document.querySelector(".perg-marg").style.margin = "75px 0 0 15px";
+      document.querySelector(".img-footer").style.top = "1750px";
+    }
+  }  
+    
+  function toggleDiv1() {
+  
+
+    if(document.getElementById("div2").style.display == 'none') {
+      document.getElementById("div2").style.display = 'block';
+      document.getElementById("div3").style.display = 'none';
+      document.querySelector(".perg-marg").style.top = "35px 0 0 15px";
+      document.querySelector(".img-footer").style.top = "1750px";
+
+      
+      
+      
+    } else {
+      document.getElementById("div2").style.display = 'none';
+      document.getElementById("div3").style.display = 'block';
+      document.querySelector(".perg-marg").style.margin = "75px 0 0 15px";
+      document.querySelector(".img-footer").style.top = "1750px";
+    }
+  }
